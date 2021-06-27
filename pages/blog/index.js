ee5440eb2be07b79873fd3ce5ab6bf6cfa938107
Haseeb Iqbal <haseebiqbal33@gmail.com>
2021-06-27 18:18:39 +0500

    Initial Commit

---
+++ pages/blog/index.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import cl from 'classnames'
+import { blogs, tabs } from '../../blogs';
+
+export default function Blog() {
+  const [selectedTabs, setSelectedTabs] = useState({});
+  const [filteredBlog, setFilteredBlog] = useState([...blogs])
+
+  const selectTab = (tab) => {
+    if (selectedTabs[tab]) delete selectedTabs[tab];
+    else selectedTabs[tab] = true;
+
+    const selectedKeys = Object.keys(selectedTabs);
+    const _blogs = [];
+
+    blogs.forEach(b => {
+      const found = b.categories.some(c => selectedKeys.includes(c));
+      if (found) _blogs.push(b);
+    })
+
+    setFilteredBlog(_blogs.length ? _blogs : blogs);
+    setSelectedTabs({...selectedTabs});
+  }
+  
+  return (
+    <div style={{ padding: '32px' }}>
+      <h1>The Undone Blog</h1>
+      <p>Tips, stories, and our musings on everything in between.</p>
+      <div style={{ marginTop: '24px' }}>
+        <div className="container">
+          <div className="tabs-container">
+            {tabs.map(t => {
+              const selectedTab = selectedTabs[t];
+              return (
+                <div key={t} className={cl("tab", { 'active': selectedTab })} onClick={() => selectTab(t)}>
+                  {t}
+                </div>
+              )
+            })}
+          </div>
+          
+          <div className="row">
+            {filteredBlog.map((blog, i) => {
+              return (
+                <div className="card" key={blog.title + i}>
+                  <div className='media' />
+                  <div className="card-header-">
+                    <h2>React</h2>
+                  </div>
+                  <div className="card-body">
+                    <p>{blog.title}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div> 
+      </div>
+    </div>
+  );
+}
